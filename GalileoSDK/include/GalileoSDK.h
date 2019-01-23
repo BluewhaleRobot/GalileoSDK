fa20707b1765ca2ad9761de4e27cbc04666b80f4
Zhang Weijia <weijiz@outlook.com>
2019-01-23 21:36:54 +0800

    add connect

--- GalileoSDK/include/GalileoSDK.h
+++ GalileoSDK/include/GalileoSDK.h
@@ -2,7 +2,6 @@
 #ifndef __GALILEO_SDK_H__
 #define __GALILEO_SDK_H__
 
-#include "stdafx.h"
 #include <iostream>
 
 #ifdef _WIN32
@@ -18,6 +17,7 @@
 #include "models/ServerInfo.h"
 #include "tasks/BroadcastReceiver.h"
 #include <thread>
+#include <mutex>
 
 namespace GalileoSDK {
 
@@ -27,6 +27,8 @@ namespace GalileoSDK {
 		INVALIDE_STATE, // 当前状态无法执行此操作
 		NO_SERVER_FOUND, // 没有发现机器人
 		MULTI_SERVER_FOUND, // 发现多个机器人
+		NETWORK_ERROR, //网络环境错误
+		ALREADY_CONNECTED, // 已经连接过了
 	};
 
 	class DLL_PUBLIC GalileoSDK {
@@ -40,9 +42,19 @@ namespace GalileoSDK {
 		GALILEO_RETURN_CODE WaitForStartNavigation(GALILEO_RETURN_CODE(*OnConnect)(std::string));
 		//DLL_PUBLIC std::vector<ServerInfo>  GetServersOnline();
 		std::vector<ServerInfo> GetServersOnline();
-		ServerInfo currentServer;
+		ServerInfo* currentServer;
+		GALILEO_RETURN_CODE PublishTest();
+		GALILEO_RETURN_CODE GetCurrentStatus(galileo_serial_server::GalileoStatusPtr status);
+		~GalileoSDK();
+
 	private:
 		BroadcastReceiver broadcastReceiver;
+		ros::NodeHandle* nh;
+		ros::Publisher testPub;
+		ros::Subscriber galileoStatusSub;
+		void UpdateGalileoStatus(const galileo_serial_server::GalileoStatusConstPtr &status);
+		galileo_serial_server::GalileoStatusPtr currentStatus;
+		std::mutex statusLock;
 	};
 
 	extern "C" DLL_PUBLIC GalileoSDK* GetSDK();
