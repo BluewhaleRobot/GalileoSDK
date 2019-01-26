6bc107ddfb8d7e1c84a5f9639a3c80b800896366
Zhang Weijia <weijiz@outlook.com>
2019-01-26 11:28:29 +0800

    format

--- GalileoSDK/include/tasks/BroadcastReceiver.h
+++ GalileoSDK/include/tasks/BroadcastReceiver.h
@@ -5,38 +5,39 @@
 #include <vector>
 #ifdef _WIN32
 #include <winsock2.h>
-#pragma comment(lib,"ws2_32.lib")
+#pragma comment(lib, "ws2_32.lib")
 #endif
 #define BUFLEN 512 * 1024
 #define BROADCAST_PORT 22002
-#include "ros/ros.h"
-#include "models/ServerInfo.h"
-#include <json/json.h>
-#include <regex>
-#include <iostream>
-#include <Ws2tcpip.h>
 #include "Utils.h"
+#include "models/ServerInfo.h"
+#include "ros/ros.h"
+#include <Ws2tcpip.h>
+#include <iostream>
+#include <json/json.h>
 #include <mutex>
+#include <regex>
 
 namespace GalileoSDK {
-	class GalileoSDK;
-	class BroadcastReceiver {
-	public:
-		BroadcastReceiver();
-		static void StopAll();
-		void StopTask();
-		static std::vector<ServerInfo> GetServers();
-		void Run();
-		void SetSDK(GalileoSDK*);
-	private:
-		SOCKET serverSocket;
-		struct sockaddr_in server, si_other;
-		bool runningFlag;
-		std::vector<ServerInfo> serverList;
-		static BroadcastReceiver* instance;
-		GalileoSDK* sdk;
-		std::mutex serversLock;
-	};
-}
+class GalileoSDK;
+class BroadcastReceiver {
+public:
+  BroadcastReceiver();
+  static void StopAll();
+  void StopTask();
+  static std::vector<ServerInfo> GetServers();
+  void Run();
+  void SetSDK(GalileoSDK *);
+
+private:
+  SOCKET serverSocket;
+  struct sockaddr_in server, si_other;
+  bool runningFlag;
+  std::vector<ServerInfo> serverList;
+  static BroadcastReceiver *instance;
+  GalileoSDK *sdk;
+  std::mutex serversLock;
+};
+} // namespace GalileoSDK
 
 #endif // !__BROADCAST_RECEIVER_H__
--- GalileoSDK/include/utils.h
+++ GalileoSDK/include/utils.h
@@ -2,49 +2,53 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
-#include <chrono>
 #include <Iphlpapi.h>
+#include <chrono>
 #include <mutex>
 #pragma comment(lib, "IPHLPAPI.lib")
 
-namespace GalileoSDK{
-	class Utils {
-	public:
-		static __int64 GetCurrentTimestamp() {
-			return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-		}
+namespace GalileoSDK {
+class Utils {
+public:
+  static __int64 GetCurrentTimestamp() {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(
+               std::chrono::system_clock::now().time_since_epoch())
+        .count();
+  }
 
-		static std::vector<std::string> ListIpAddresses() {
+  static std::vector<std::string> ListIpAddresses() {
 
-			std::vector<std::string> myIPs;
-		
-			PIP_ADAPTER_INFO pAdapterInfo;
-			pAdapterInfo = (IP_ADAPTER_INFO *)malloc(sizeof(IP_ADAPTER_INFO));
-			ULONG buflen = sizeof(IP_ADAPTER_INFO);
+    std::vector<std::string> myIPs;
 
-			if (GetAdaptersInfo(pAdapterInfo, &buflen) == ERROR_BUFFER_OVERFLOW) {
-				free(pAdapterInfo);
-				pAdapterInfo = (IP_ADAPTER_INFO *)malloc(buflen);
-			}
+    PIP_ADAPTER_INFO pAdapterInfo;
+    pAdapterInfo = (IP_ADAPTER_INFO *)malloc(sizeof(IP_ADAPTER_INFO));
+    ULONG buflen = sizeof(IP_ADAPTER_INFO);
 
-			if (GetAdaptersInfo(pAdapterInfo, &buflen) == NO_ERROR) {
-				PIP_ADAPTER_INFO pAdapter = pAdapterInfo;
-				while (pAdapter) {
-					std::string desp = std::string(pAdapter->Description);
-					if (desp.find("Virtual") != std::string::npos || desp.find("Loopback") != std::string::npos
-						|| desp.find("virtual") != std::string::npos || desp.find("loopback") != std::string::npos) {
-						pAdapter = pAdapter->Next;
-						continue;
-					}
-					myIPs.push_back(pAdapter->IpAddressList.IpAddress.String);
-					pAdapter = pAdapter->Next;
-				}
-			}
+    if (GetAdaptersInfo(pAdapterInfo, &buflen) == ERROR_BUFFER_OVERFLOW) {
+      free(pAdapterInfo);
+      pAdapterInfo = (IP_ADAPTER_INFO *)malloc(buflen);
+    }
 
-			return myIPs;
-		}
-	};
+    if (GetAdaptersInfo(pAdapterInfo, &buflen) == NO_ERROR) {
+      PIP_ADAPTER_INFO pAdapter = pAdapterInfo;
+      while (pAdapter) {
+        std::string desp = std::string(pAdapter->Description);
+        if (desp.find("Virtual") != std::string::npos ||
+            desp.find("Loopback") != std::string::npos ||
+            desp.find("virtual") != std::string::npos ||
+            desp.find("loopback") != std::string::npos) {
+          pAdapter = pAdapter->Next;
+          continue;
+        }
+        myIPs.push_back(pAdapter->IpAddressList.IpAddress.String);
+        pAdapter = pAdapter->Next;
+      }
+    }
 
-}
+    return myIPs;
+  }
+};
+
+} // namespace GalileoSDK
 
 #endif // !__UTILS_H__
